d2a402cb07185490a53caf52ba79e659b5c36909
Keerthana <Keerthana@gmail.com>
2026-03-14 01:08:55 +0530

    Added blockchain

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,31 +1,40 @@
-const crypto = require("crypto")
+const crypto = require('crypto');
 
-class Block{
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.nonce = 0;
+        this.hash = this.calculateHash();
+    }
 
-constructor(index,timestamp,data,previousHash){
+    calculateHash() {
+        return crypto.createHash('sha256')
+            .update(
+                this.index +
+                this.previousHash +
+                this.timestamp +
+                JSON.stringify(this.data) +
+                this.nonce
+            )
+            .digest('hex');
+    }
 
-this.index=index
-this.timestamp=timestamp
-this.data=data
-this.previousHash=previousHash
-this.hash=this.calculateHash()
-
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('\n========================================');
+        console.log('🔗 BLOCK MINED!');
+        console.log('========================================');
+        console.log('Block Hash:', this.hash);
+        console.log('Previous Hash:', this.previousHash);
+        console.log('Vote Data:', JSON.stringify(this.data, null, 2));
+        console.log('========================================\n');
+    }
 }
 
-calculateHash(){
-
-return crypto
-.createHash("sha256")
-.update(
-this.index+
-this.timestamp+
-JSON.stringify(this.data)+
-this.previousHash
-)
-.digest("hex")
-
-}
-
-}
-
-module.exports=Block
+module.exports = Block;
--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,32 +1,67 @@
-const Block=require("./block")
+const Block = require('./block');
 
-class Blockchain{
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingVotes = [];
+    }
 
-constructor(){
-this.chain=[this.createGenesisBlock()]
+    createGenesisBlock() {
+        return new Block(0, Date.now(), { message: 'Genesis Block - SarvaMat Voting System' }, '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addVote(voteData) {
+        const newBlock = new Block(
+            this.chain.length,
+            Date.now(),
+            voteData,
+            this.getLatestBlock().hash
+        );
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+        return newBlock;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    getVoteCount() {
+        const counts = {};
+        for (let i = 1; i < this.chain.length; i++) {
+            const candidate = this.chain[i].data.candidate;
+            if (candidate) {
+                counts[candidate] = (counts[candidate] || 0) + 1;
+            }
+        }
+        return counts;
+    }
+
+    getAllVotes() {
+        return this.chain.slice(1).map(block => ({
+            hash: block.hash,
+            voter: block.data.visibleVoterId,
+            candidate: block.data.candidate,
+            timestamp: block.timestamp
+        }));
+    }
 }
 
-createGenesisBlock(){
-return new Block(0,Date.now(),"Genesis Block","0")
-}
-
-getLatestBlock(){
-return this.chain[this.chain.length-1]
-}
-
-addBlock(data){
-
-const newBlock=new Block(
-this.chain.length,
-Date.now(),
-data,
-this.getLatestBlock().hash
-)
-
-this.chain.push(newBlock)
-
-}
-
-}
-
-module.exports=Blockchain
+module.exports = Blockchain;
